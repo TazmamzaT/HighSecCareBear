6270f99127dc71560fcdf361e072e1b855e82dd0
TazmamzaT <tazmamzat@gmail.com>
2021-09-12 20:12:41 -0500

    Change post deploy to reference the import scripts so the publish scripts aren't super messy at the bottom

--- HighSecCareBear.Database/Scripts/Script.PostDeployment1.sql
+++ HighSecCareBear.Database/Scripts/Script.PostDeployment1.sql
@@ -10,15 +10,7 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
-IF (EXISTS(SELECT * FROM [Ore].[OreInfo]))  
+IF (NOT EXISTS(SELECT * FROM [Ore].[OreInfo]))  
 BEGIN  
-    BULK INSERT [Ore].[OreInfo]
-    FROM '..\Imports\Ore.csv'
-    WITH
-    (
-        FIRSTROW = 2, -- as 1st one is header
-        FIELDTERMINATOR = ',',  --CSV field delimiter
-        ROWTERMINATOR = '\n',   --Use to shift the control to next row
-        TABLOCK
-    )
+    :r ..\Imports\ImportOreTypes.sql
 END  
