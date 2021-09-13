c7c87fd40c90f5661e221a8f825a0aa84096af9a
TazmamzaT <tazmamzat@gmail.com>
2021-09-12 20:10:50 -0500

    Add identity to OreInfo Id. Add extra headroom for column decimals.

--- HighSecCareBear.Database/Ore/Tables/OreInfo.sql
+++ HighSecCareBear.Database/Ore/Tables/OreInfo.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [Ore].[OreInfo]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT IDENTITY(1,1) PRIMARY KEY, 
     [Name] NCHAR(30) NOT NULL, 
-    [Volume] DECIMAL(2, 2) NOT NULL, 
-    [Modifier] DECIMAL(2,2) NOT NULL, 
-    [TypeId] INT NOT NULL
+    [Volume] DECIMAL(6, 2) NOT NULL, 
+    [Modifier] DECIMAL(3,2) NOT NULL, 
+    [TypeId] INT NOT NULL 
 )
