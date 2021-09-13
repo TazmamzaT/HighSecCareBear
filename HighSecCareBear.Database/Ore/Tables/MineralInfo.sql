ba6dc220a54986ed0c0874023d471995043947a8
TazmamzaT <tazmamzat@gmail.com>
2021-09-12 20:09:48 -0500

    Add identity to Id column in Mineral column

--- HighSecCareBear.Database/Ore/Tables/MineralInfo.sql
+++ HighSecCareBear.Database/Ore/Tables/MineralInfo.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [Ore].[MineralInfo]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT IDENTITY(1,1) PRIMARY KEY, 
     [Name] NCHAR(30) NOT NULL, 
     [Voume] DECIMAL(2, 2) NOT NULL, 
     [TypeId] INT NOT NULL
