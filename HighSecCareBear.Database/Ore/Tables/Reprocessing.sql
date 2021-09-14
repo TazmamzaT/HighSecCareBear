f5e8dfbf21afeede919db3d3492dd225c0077fc5
TazmamzaT <tazmamzat@gmail.com>
2021-09-13 19:18:28 -0500

    Remove import table. Remove CSVs. Add MineralInfo and Reporcessing populates

--- HighSecCareBear.Database/Ore/Tables/Reprocessing.sql
+++ HighSecCareBear.Database/Ore/Tables/Reprocessing.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [Ore].[Reprocessing]
 (
-	[Id] INT IDENTITY(1,1) PRIMARY KEY, 
     [OreId] INT NOT NULL, 
     [MineralId] INT NOT NULL, 
     [Yield100] INT NOT NULL, 
     CONSTRAINT [FK_Reprocessing_OreInfo] FOREIGN KEY ([OreId]) REFERENCES [Ore].[OreInfo]([Id]), 
-    CONSTRAINT [FK_Reprocessing_MineralInfo] FOREIGN KEY (MineralId) REFERENCES [Ore].[MineralInfo]([Id]) 
+    CONSTRAINT [FK_Reprocessing_MineralInfo] FOREIGN KEY (MineralId) REFERENCES [Ore].[MineralInfo]([Id]), 
+    CONSTRAINT [PK_Reprocessing] PRIMARY KEY ([OreId], [MineralId]) 
 )
